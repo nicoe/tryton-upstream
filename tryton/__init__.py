9999f401428308d9f16fd21dd819d89c407db48f
Jean CAVALLO <jean.cavallo@coopengo.com>
2014-01-31 11:11:23 +0100

    Rule engine initial commit [CUSTOM]

--- tryton/__init__.py
+++ tryton/__init__.py
@@ -7,6 +7,7 @@ import locale
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
 gi.require_foreign('cairo')
+gi.require_version('GtkSource', '3.0')
 try:
     gi.require_version('GtkSpell', '3.0')
 except ValueError:
